f4ed235fe9060d71318f977680e30173f6bc479c
rickyriosp <riki.rios91@gmail.com>
2020-07-28 00:07:09 -0400

    Updates

---
+++ precache-manifest.2149fa01f2ad8ff6218e9020ee31c4be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33961145f67bccc307cb1342fae9a6d0",
+    "url": "/rickyriosp/face-recognition-app/index.html"
+  },
+  {
+    "revision": "1a985bf89694d3c255b4",
+    "url": "/rickyriosp/face-recognition-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d16ec5e77ce57ed1ffe9",
+    "url": "/rickyriosp/face-recognition-app/static/css/main.ac8762e2.chunk.css"
+  },
+  {
+    "revision": "1a985bf89694d3c255b4",
+    "url": "/rickyriosp/face-recognition-app/static/js/2.75c43936.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/rickyriosp/face-recognition-app/static/js/2.75c43936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16ec5e77ce57ed1ffe9",
+    "url": "/rickyriosp/face-recognition-app/static/js/main.419dc562.chunk.js"
+  },
+  {
+    "revision": "b331d47dfde6cc4ddfc4",
+    "url": "/rickyriosp/face-recognition-app/static/js/runtime-main.4098d20d.js"
+  }
+]);
